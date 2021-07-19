6ecdb39ffc735c1e3d87298a97d2a0b82e96416e
ChihChengLiang <chihchengliang@gmail.com>
2021-07-20 00:22:43 +0800

    add u8s to u256

--- src/encoding/main.py
+++ src/encoding/main.py
@@ -13,3 +13,10 @@ def is_circuit_code(func):
 def u256_to_u8s(x: U256) -> Tuple[U8, ...]:
     assert 0 <= x < 2**256, "expect x is unsigned 256 bits"
     return tuple((x >> 8*i) & 0xff for i in range(32))
+
+
+def u8s_to_u256(xs: Sequence[U8]) -> U256:
+    assert len(xs) == 32
+    for u8 in xs:
+        assert 0 <= u8 <= 255
+    return sum(x * (2**(8*i)) for i, x in enumerate(xs))
--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -1,11 +1,12 @@
-from encoding import u256_to_u8s
+from encoding import u256_to_u8s, u8s_to_u256
 import pytest
 
 
-@pytest.mark.parametrize("test_input,expected", (
+@pytest.mark.parametrize("u256,u8s", (
     (1, (1, ) + (0, )*31),
     ((1 << 256) - 1, (255,) * 32),
-
+    (1 << 248, (0, )*31 + (1, )),
 ))
-def test_u256_to_u8s(test_input, expected):
-    assert u256_to_u8s(test_input) == expected
+def test_u256_and_u8s_conversion(u256, u8s):
+    assert u256_to_u8s(u256) == u8s
+    assert u8s_to_u256(u8s) == u256
