5e6008beac05d5106432bd98dcf91f282d3b0992
han0110 <tinghan0110@gmail.com>
2021-10-29 13:58:15 +0800

    fix: add more comment on call_id propagation and fix comment in error cases

--- src/zkevm_specs/evm/execution_result/error_stack_underflow.py
+++ src/zkevm_specs/evm/execution_result/error_stack_underflow.py
@@ -6,4 +6,4 @@ from ..table import FixedTableTag
 def error_stack_underflow(curr: Step, next: Step, r: int, opcode: Opcode):
     curr.fixed_lookup(FixedTableTag.StackUnderflow, [opcode, curr.call.stack_pointer])
 
-    # TODO: Return to caller's state or go to next tx
+    # TODO: Return to caller's state
