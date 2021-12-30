8fe532a5285d83b082488c2a97ddf288ed0c9ffd
HAOYUatHZ <haoyu@protonmail.com>
2021-12-30 16:53:51 +0800

    improve

--- src/zkevm_specs/opcode/sload_sstore.py
+++ src/zkevm_specs/opcode/sload_sstore.py
@@ -1,8 +1,11 @@
+from zkevm_specs.opcode.storage import Storage
+
 OP_SLOAD = 0x54
 OP_SSTORE = 0x55
 
 @is_circuit_code
 def check_storage_ops(
     opcode: U8,
+    storage: Storage,
 ):
-    TODO:
+    # TODO:
