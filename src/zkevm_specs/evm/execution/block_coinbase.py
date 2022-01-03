1789e74d7e69b75614e267a7e2d43394b925d881
Haichen Shen <shenhaichen@gmail.com>
2022-01-03 15:18:03 -0800

    lint

--- src/zkevm_specs/evm/execution/block_coinbase.py
+++ src/zkevm_specs/evm/execution/block_coinbase.py
@@ -9,8 +9,7 @@ def coinbase(instruction: Instruction):
     address = instruction.stack_push()
     # in real circuit also check address raw data is 160 bit length (20 bytes)
     # check block table for coinbase address
-    instruction.constrain_equal(address,
-        instruction.block_lookup(BlockContextFieldTag.Coinbase))
+    instruction.constrain_equal(address, instruction.block_lookup(BlockContextFieldTag.Coinbase))
 
     instruction.constrain_same_context_state_transition(
         opcode,
