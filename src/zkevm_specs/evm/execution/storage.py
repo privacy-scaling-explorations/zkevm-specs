60a0e1ed692fe743a51c947549501ac22c92d86f
HAOYUatHZ <haoyu@protonmail.com>
2022-01-04 13:37:51 +0800

    update

--- src/zkevm_specs/evm/execution/storage.py
+++ src/zkevm_specs/evm/execution/storage.py
@@ -1,5 +1,9 @@
 from ..instruction import Instruction, Transition
 
+# TODO:
+# constraint op?
+# combine to storage?
+
 def sload(instruction: Instruction):
     opcode = instruction.opcode_lookup(True)
     instruction.constrain_equal(opcode, Opcode.SLOAD)
@@ -10,5 +14,9 @@ def sload(instruction: Instruction):
 def sstore(instruction: Instruction):
     opcode = instruction.opcode_lookup(True)
     instruction.constrain_equal(opcode, Opcode.SSTORE)
+
     address = instruction.stack_pop()
     value = instruction.stack_pop()
+
+    rw_counter_end_of_reversion = instruction.call_context_lookup(CallContextFieldTag.RWCounterEndOfReversion)
+    is_persistent = instruction.call_context_lookup(CallContextFieldTag.IsPersistent)
