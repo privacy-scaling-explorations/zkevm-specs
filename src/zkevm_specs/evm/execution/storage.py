668acc8c895afdf08bd1dc16a5b48d0007cbd176
HAOYUatHZ <haoyu@protonmail.com>
2022-01-04 19:48:01 +0800

    minor

--- src/zkevm_specs/evm/execution/storage.py
+++ src/zkevm_specs/evm/execution/storage.py
@@ -29,18 +29,21 @@ def sstore(instruction: Instruction):
     instruction.constrain_equal(opcode, Opcode.SSTORE)
 
     tx_id = instruction.call_context_lookup(CallContextFieldTag.TxId)
-    rw_counter_end_of_reversion = instruction.call_context_lookup(CallContextFieldTag.RWCounterEndOfReversion)
-    is_persistent = instruction.call_context_lookup(CallContextFieldTag.IsPersistent)
-
     account_address = instruction.tx_lookup(tx_id, TxContextFieldTag.CalleeAddress)
+    is_persistent = instruction.call_context_lookup(CallContextFieldTag.IsPersistent)
+    rw_counter_end_of_reversion = instruction.call_context_lookup(CallContextFieldTag.RWCounterEndOfReversion)
 
     storage_slot = instruction.stack_pop()
     value = instruction.stack_pop()
 
     _ = self.add_storage_slot_to_access_list_with_reversion(
-        account_address, storage_slot,
-        is_persistent, rw_counter_end_of_reversion
+        tx_id, account_address, storage_slot, is_persistent, rw_counter_end_of_reversion
     )
 
     # TDOO: deal with gas correctly
-
+    instruction.constrain_same_context_state_transition(
+        opcode,
+        rw_counter=Transition.delta(3),
+        program_counter=Transition.delta(1),
+        stack_pointer=Transition.delta(0),
+    )
