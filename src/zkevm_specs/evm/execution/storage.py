d6e315a248e8e9f0e54d658d1a5c05066b8497d6
HAOYUatHZ <haoyu@protonmail.com>
2022-01-05 13:51:25 +0800

    fix sload

--- src/zkevm_specs/evm/execution/storage.py
+++ src/zkevm_specs/evm/execution/storage.py
@@ -7,16 +7,17 @@ def sload(instruction: Instruction):
     instruction.constrain_equal(opcode, Opcode.SLOAD)
 
     tx_id = instruction.call_context_lookup(CallContextFieldTag.TxId)
-    account_address = instruction.tx_lookup(tx_id, TxContextFieldTag.CalleeAddress)
+    callee_address = instruction.tx_lookup(tx_id, TxContextFieldTag.CalleeAddress)
 
     storage_slot = instruction.stack_pop()
-    instruction.storage_read(tx_id, account_address, storage_slot)
+    instruction.storage_slot_read(callee_address, storage_slot)
+    instruction.add_storage_slot_to_access_list(tx_id, callee_address, storage_slot)
     value = instruction.stack_push()
 
     # TDOO: deal with gas correctly
     instruction.constrain_same_context_state_transition(
         opcode,
-        rw_counter=Transition.delta(3),
+        rw_counter=Transition.delta(4),
         program_counter=Transition.delta(1),
         stack_pointer=Transition.delta(0),
     )
@@ -27,21 +28,22 @@ def sstore(instruction: Instruction):
     instruction.constrain_equal(opcode, Opcode.SSTORE)
 
     tx_id = instruction.call_context_lookup(CallContextFieldTag.TxId)
-    account_address = instruction.tx_lookup(tx_id, TxContextFieldTag.CalleeAddress)
+    callee_address = instruction.tx_lookup(tx_id, TxContextFieldTag.CalleeAddress)
     is_persistent = instruction.call_context_lookup(CallContextFieldTag.IsPersistent)
     rw_counter_end_of_reversion = instruction.call_context_lookup(CallContextFieldTag.RWCounterEndOfReversion)
 
     storage_slot = instruction.stack_pop()
     value = instruction.stack_pop()
+    # TODO: reversion?
     self.add_storage_slot_to_access_list_with_reversion(
-        tx_id, account_address, storage_slot, is_persistent, rw_counter_end_of_reversion
+        tx_id, callee_address, storage_slot, is_persistent, rw_counter_end_of_reversion
     )
 
     # TDOO: deal with gas correctly
     # TODO: constrain_new_context_state_transition?
     instruction.constrain_same_context_state_transition(
         opcode,
-        rw_counter=Transition.delta(3),
+        rw_counter=Transition.delta(4),
         program_counter=Transition.delta(1),
         stack_pointer=Transition.delta(-2),
     )
