50d233adab68c2cdff4be7e973f726447ce529df
HAOYUatHZ <haoyu@protonmail.com>
2022-01-10 16:45:27 +0800

    fix sload dynamic_gas_cost

--- src/zkevm_specs/evm/execution/storage.py
+++ src/zkevm_specs/evm/execution/storage.py
@@ -2,6 +2,13 @@ from ..instruction import Instruction, Transition
 from ..opcode import Opcode
 from ..table import CallContextFieldTag, TxContextFieldTag
 
+COLD_SLOAD_COST = 2100
+WARM_STORAGE_READ_COST = 100
+SLOAD_GAS = 100
+SSTORE_SET_GAS = 20000
+SSTORE_RESET_GAS = 2900
+SSTORE_CLEARS_SCHEDULE = 15000
+
 def sload(instruction: Instruction):
     opcode = instruction.opcode_lookup(True)
     instruction.constrain_equal(opcode, Opcode.SLOAD)
@@ -12,10 +19,10 @@ def sload(instruction: Instruction):
     callee_address = instruction.tx_lookup(tx_id, TxContextFieldTag.CalleeAddress)
 
     storage_slot = instruction.stack_pop()
-    instruction.access_list_storage_slot_read(tx_id, callee_address, storage_slot)
+    warm = instruction.access_list_storage_slot_read(tx_id, callee_address, storage_slot)
 
     # TODO: Use intrinsic gas (EIP 2028, 2930)
-    dynamic_gas_cost = 0
+    dynamic_gas_cost = WARM_STORAGE_READ_COST if warm else COLD_SLOAD_COST
 
     instruction.storage_slot_read(callee_address, storage_slot)
     instruction.add_storage_slot_to_access_list_with_reversion(
