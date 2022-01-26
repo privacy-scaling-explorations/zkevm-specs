32c1d440314057e5928dcdc5e18f95b526a94ea8
Rohit Narurkar <rohit.narurkar@protonmail.com>
2022-01-26 10:11:23 +0800

    fix: rw table entry for stack write

--- tests/evm/test_gas.py
+++ tests/evm/test_gas.py
@@ -38,7 +38,7 @@ def test_gas(gas: int):
         bytecode_table=set(bytecode.table_assignments(randomness)),
         rw_table=set(
             [
-                (2, RW.Write, RWTableTag.Stack, 1, 1023, gas_left, 0, 0),
+                (2, RW.Write, RWTableTag.Stack, 1, 1023, gas_left, 0, 0, 0),
             ]
         ),
     )
