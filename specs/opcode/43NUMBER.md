6c1a8c88bdf3a2b8d0239048ebb95733c5f36547
huyanlin <bchylmn@gmail.com>
2022-03-04 10:27:46 +0800

    add spec of op number

---
+++ specs/opcode/43NUMBER.md
@@ -0,0 +1,36 @@
+# Number op code
+
+## Procedure
+
+The `NUMBER` opcode get the block number from current block context and then push it onto the stack.
+
+## EVM behavior
+
+The `NUMBER` opcode loads a `number` (32 bytes of data) from the block context and then
+pushes it onto the stack.
+
+## Circuit behavior
+
+1. construct block context table
+2. do busmapping lookup for stack write operation
+
+## Constraints
+
+1. opId = 0x43
+2. State transition:
+   - gc + 1 (1 stack write)
+   - stack_pointer - 1
+   - pc + 1
+   - gas + 2
+3. Lookups:  2
+   - `number` is on the top of stack
+   - `number` is in the block context table
+
+## Exceptions
+
+1. stack overflow: stack is full, stack pointer = 0
+2. out of gas: remaining gas is not enough
+
+## Code
+
+Please refer to `src/zkevm_specs/evm/execution/block_number.py`.
---
+++ tests/evm/test_number.py
@@ -0,0 +1,66 @@
+import pytest
+
+from zkevm_specs.evm import (
+    ExecutionState,
+    StepState,
+    verify_steps,
+    Tables,
+    RWTableTag,
+    RW,
+    Block,
+    Bytecode,
+)
+from zkevm_specs.util import rand_address, rand_fp, RLC, U256, rand_word
+
+
+TESTING_DATA = (0, 1, 2**256 - 1, rand_word())
+
+
+@pytest.mark.parametrize("number", TESTING_DATA)
+def test_number(number: U256):
+    randomness = rand_fp()
+
+    block = Block(number=number)
+
+    bytecode = Bytecode().number()
+    bytecode_hash = RLC(bytecode.hash(), randomness)
+
+    tables = Tables(
+        block_table=set(block.table_assignments(randomness)),
+        tx_table=set(),
+        bytecode_table=set(bytecode.table_assignments(randomness)),
+        rw_table=set(
+            [
+                (9, RW.Write, RWTableTag.Stack, 1, 1023, 0, RLC(number, randomness), 0, 0, 0),
+            ]
+        ),
+    )
+
+    verify_steps(
+        randomness=randomness,
+        tables=tables,
+        steps=[
+            StepState(
+                execution_state=ExecutionState.NUMBER,
+                rw_counter=9,
+                call_id=1,
+                is_root=True,
+                is_create=False,
+                code_source=bytecode_hash,
+                program_counter=0,
+                stack_pointer=1024,
+                gas_left=2,
+            ),
+            StepState(
+                execution_state=ExecutionState.STOP,
+                rw_counter=10,
+                call_id=1,
+                is_root=True,
+                is_create=False,
+                code_source=bytecode_hash,
+                program_counter=1,
+                stack_pointer=1023,
+                gas_left=0,
+            ),
+        ],
+    )
