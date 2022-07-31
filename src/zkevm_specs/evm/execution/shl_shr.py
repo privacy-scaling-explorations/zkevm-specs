c053421395def3d88da78eb5f2a3ed38c28c06d5
Steven Gu <asongala@163.com>
2022-07-31 08:36:57 +0800

    Update `SHL & SHR` implementation.

--- src/zkevm_specs/evm/execution/shl_shr.py
+++ src/zkevm_specs/evm/execution/shl_shr.py
@@ -1,4 +1,4 @@
-from ...util import RLC
+from ...util import FQ, RLC
 from ..instruction import Instruction, Transition
 from ..opcode import Opcode
 
@@ -6,32 +6,114 @@ from ..opcode import Opcode
 def shl_shr(instruction: Instruction):
     opcode = instruction.opcode_lookup(True)
 
-    a = instruction.stack_pop()
-    shift = instruction.stack_pop()
-    b = instruction.stack_push()
+    pop1 = instruction.stack_pop()
+    pop2 = instruction.stack_pop()
+    push = instruction.stack_push()
 
-    is_shl = Opcode.SHR - opcode
-    shf0 = instruction.bytes_to_fq(shift.le_bytes[:1])
-    shf_lt256 = instruction.is_zero(instruction.sum(shift.le_bytes[1:]))
-
-    if is_shl == 1:
-        dividend = b
-        divisor = instruction.select(shf_lt256, RLC(1 << shf0.n), RLC(0))
-        quotient = a
-        remainder = RLC(0)
-    else:
-        dividend = a
-        divisor = instruction.select(shf_lt256, RLC(1 << shf0.n), RLC(0))
-        quotient = b
-        remainder = instruction.rlc_encode(
-            dividend.int_value - divisor.int_value * quotient.int_value, 32
-        )
-
-    instruction.mul_add_words(quotient, divisor, remainder, dividend)
+    (
+        is_shl,
+        shf0,
+        shift,
+        dividend,
+        divisor,
+        quotient,
+        remainder,
+    ) = gen_witness(opcode, pop1, pop2, push)
+    check_witness(
+        instruction,
+        is_shl,
+        shf0,
+        shift,
+        dividend,
+        divisor,
+        quotient,
+        remainder,
+        pop1,
+        pop2,
+        push,
+    )
 
     instruction.step_state_transition_in_same_context(
         opcode,
-        rw_counter=Transition.delta(2),
+        rw_counter=Transition.delta(3),
         program_counter=Transition.delta(1),
         stack_pointer=Transition.delta(1),
+    )
+
+
+def check_witness(
+    instruction: Instruction,
+    is_shl: FQ,
+    shf0: FQ,
+    shift: RLC,
+    dividend: RLC,
+    divisor: RLC,
+    quotient: RLC,
+    remainder: RLC,
+    pop1: RLC,
+    pop2: RLC,
+    push: RLC,
+):
+    is_shr = 1 - is_shl
+    divisor_is_zero = instruction.word_is_zero(divisor)
+
+    # Constrain stack pops and pushes as:
+    # - for SHL, two pops are shift and quotient, and push is dividend.
+    # - for SHR, two pops are shift and dividend, and push is quotient.
+    instruction.constrain_equal(pop1.expr(), shift.expr())
+    instruction.constrain_equal(
+        pop2.expr(),
+        is_shl * quotient.expr() + is_shr * dividend.expr(),
+    )
+    instruction.constrain_equal(
+        push.expr(), is_shl * dividend.expr() + is_shr * quotient.expr() * (1 - divisor_is_zero)
+    )
+
+    # Constrain remainder < divisor when divisor != 0.
+    remainder_lt_divisor, _ = instruction.compare_word(remainder, divisor)
+    instruction.constrain_zero((1 - divisor_is_zero) * (1 - remainder_lt_divisor))
+
+    # Constrain remainder == 0 for SHL.
+    remainder_is_zero = instruction.word_is_zero(remainder)
+    instruction.constrain_zero(is_shl * (1 - remainder_is_zero))
+
+    # Constrain overflow == 0 for SHR.
+    overflow = instruction.mul_add_words(quotient, divisor, remainder, dividend)
+    instruction.constrain_zero(is_shr * overflow)
+
+    # Constrain pop1 == pop1.cells[0] when divisor != 0.
+    instruction.constrain_zero(
+        (1 - divisor_is_zero) * (pop1.expr() - pop1.le_bytes[0]),
+    )
+
+    # Constrain divisor_lo == 2^shf0 when shf0 < 128, and
+    # divisor_hi == 2^(128 - shf0) otherwise.
+    divisor_lo = instruction.bytes_to_fq(divisor.le_bytes[:16])
+    divisor_hi = instruction.bytes_to_fq(divisor.le_bytes[16:])
+    if (1 - divisor_is_zero) == 1:
+        instruction.pow2_lookup(shf0, divisor_lo, divisor_hi)
+
+
+def gen_witness(opcode: FQ, pop1: RLC, pop2: RLC, push: RLC):
+    is_shl = Opcode.SHR - opcode
+    shift = pop1
+    shf0 = shift.le_bytes[0]
+    divisor = RLC(1 << shf0) if shf0 == shift.int_value else RLC(0)
+    if is_shl.n == 1:
+        dividend = push
+        quotient = pop2
+        remainder = RLC(0)
+    else:  # SHR
+        dividend = pop2
+        quotient = push
+        remainder = RLC(dividend.int_value - quotient.int_value * divisor.int_value)
+
+    return (
+        is_shl,
+        shf0,
+        shift,
+        dividend,
+        divisor,
+        quotient,
+        remainder,
     )
--- tests/evm/test_shl_shr.py
+++ tests/evm/test_shl_shr.py
@@ -10,46 +10,51 @@ from zkevm_specs.evm import (
     Tables,
     verify_steps,
 )
-from zkevm_specs.util import rand_fq, RLC, U256
+from zkevm_specs.util import rand_fq, rand_word, RLC
+from common import generate_nasty_tests
 
 
-TESTING_MAX_RLC = (1 << 256) - 1
+MAX_WORD = (1 << 256) - 1
 
-TESTING_DATA = (
-    (Opcode.SHL, 0xABCD << 240, 8),
-    (Opcode.SHL, 0x1234 << 240, 7),
-    (Opcode.SHL, 0x8765 << 240, 17),
-    (Opcode.SHL, 0x4321 << 240, 0),
-    (Opcode.SHL, 0xFFFF, 256),
-    (Opcode.SHL, 0x12345, 256 + 8 + 1),
-    (Opcode.SHL, TESTING_MAX_RLC, 63),
-    (Opcode.SHL, TESTING_MAX_RLC, 128),
-    (Opcode.SHL, TESTING_MAX_RLC, 129),
-    (Opcode.SHR, 0xABCD, 8),
-    (Opcode.SHR, 0x1234, 7),
-    (Opcode.SHR, 0x8765, 17),
-    (Opcode.SHR, 0x4321, 0),
-    (Opcode.SHR, 0xFFFF, 256),
-    (Opcode.SHR, 0x12345, 256 + 8 + 1),
-    (Opcode.SHR, (1 << 256) - 1, 63),
-    (Opcode.SHR, (1 << 256) - 1, 128),
-    (Opcode.SHR, (1 << 256) - 1, 129),
-)
+TESTING_DATA = [
+    (Opcode.SHL, 8, 0xABCD << 240),
+    (Opcode.SHL, 7, 0x1234 << 240),
+    (Opcode.SHL, 17, 0x8765 << 240),
+    (Opcode.SHL, 0, 0x4321 << 240),
+    (Opcode.SHL, 256, 0xFFFF),
+    (Opcode.SHL, 256 + 8 + 1, 0x12345),
+    (Opcode.SHL, 63, MAX_WORD),
+    (Opcode.SHL, 128, MAX_WORD),
+    (Opcode.SHL, 129, MAX_WORD),
+    (Opcode.SHR, 8, 0xABCD),
+    (Opcode.SHR, 7, 0x1234),
+    (Opcode.SHR, 17, 0x8765),
+    (Opcode.SHR, 0, 0x4321),
+    (Opcode.SHR, 256, 0xFFFF),
+    (Opcode.SHR, 256 + 8 + 1, 0x12345),
+    (Opcode.SHR, 63, (1 << 256) - 1),
+    (Opcode.SHR, 128, (1 << 256) - 1),
+    (Opcode.SHR, 129, (1 << 256) - 1),
+    (Opcode.SHL, rand_word(), rand_word()),
+    (Opcode.SHR, rand_word(), rand_word()),
+]
+
+generate_nasty_tests(TESTING_DATA, (Opcode.SHL, Opcode.SHR))
 
 
-@pytest.mark.parametrize("opcode, a, shift", TESTING_DATA)
-def test_shl_shr(opcode: Opcode, a: U256, shift: int):
+@pytest.mark.parametrize("opcode, shift, a", TESTING_DATA)
+def test_shl_shr(opcode: Opcode, shift: int, a: int):
     if opcode == Opcode.SHL:
-        b = a << shift & TESTING_MAX_RLC if shift <= 255 else 0
-    else:
+        b = a << shift & MAX_WORD if shift <= 255 else 0
+        bytecode = Bytecode().shl(shift, a)
+    else:  # SHR
         b = a >> shift if shift <= 255 else 0
+        bytecode = Bytecode().shr(shift, a)
 
     randomness = rand_fq()
-    a = RLC(a, randomness)
     shift = RLC(shift, randomness)
+    a = RLC(a, randomness)
     b = RLC(b, randomness)
-
-    bytecode = Bytecode().shl(a, shift) if opcode == Opcode.SHL else Bytecode().shr(a, shift)
     bytecode_hash = RLC(bytecode.hash(), randomness)
 
     tables = Tables(
@@ -58,8 +63,8 @@ def test_shl_shr(opcode: Opcode, a: U256, shift: int):
         bytecode_table=set(bytecode.table_assignments(randomness)),
         rw_table=set(
             RWDictionary(9)
-            .stack_read(1, 1022, a)
-            .stack_read(1, 1023, shift)
+            .stack_read(1, 1022, shift)
+            .stack_read(1, 1023, a)
             .stack_write(1, 1023, b)
             .rws
         ),
@@ -82,7 +87,7 @@ def test_shl_shr(opcode: Opcode, a: U256, shift: int):
             ),
             StepState(
                 execution_state=ExecutionState.STOP,
-                rw_counter=11,
+                rw_counter=12,
                 call_id=1,
                 is_root=True,
                 is_create=False,
