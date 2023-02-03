721a1cac38f431cb0f4ad173e3966ff7c8d061f4
Steven Gu <asongala@163.com>
2023-02-03 10:04:32 +0800

    Fix to only do fixed lookup for `ResponsibleOpcode` to verify invalid opcodes.

--- specs/error_state/ErrorInvalidOpcode.md
+++ specs/error_state/ErrorInvalidOpcode.md
@@ -11,18 +11,10 @@
 
 ### Constraints
 
-1. Current opcode must be invalid. It verifies invalid bytes in any condition of:
-   - `opcode > 0x20 && opcode < 0x30`
-   - `opcode > 0x48 && opcode < 0x50`
-   - `opcode > 0xA4 && opcode < 0xF0`
-   - one of `[0x0C, 0x0D, 0x0E, 0x0F, 0x1E, 0x1F, 0x5C, 0x5D, 0x5E, 0x5F, 0xF6, 0xF7, 0xF8, 0xF9, 0xFB, 0xFC, 0xFE]`
-2. Current call must be failed.
+1. Do a fixed lookup for `FixedTableTag.ResponsibleOpcode`.
+2. Current call must be failed. Do a call context lookup for `CallContextFieldTag.IsSuccess`.
 3. If it's a root call, it transits to `EndTx`.
 4. if it is not root call, it restores caller's context by reading to `rw_table`, then does step state transition to it.
-
-### Lookups
-
-- Call Context lookup `CallContextFieldTag.IsSuccess`.
 
 ## Code
 
--- src/zkevm_specs/evm/execution/error_invalid_opcode.py
+++ src/zkevm_specs/evm/execution/error_invalid_opcode.py
@@ -3,50 +3,12 @@ from ..execution_state import ExecutionState
 from ..instruction import Instruction, Transition
 from ..table import CallContextFieldTag
 
-SEPARATE_INVALID_OPCODES = [
-    0x0C,
-    0x0D,
-    0x0E,
-    0x0F,
-    0x1E,
-    0x1F,
-    0x5C,
-    0x5D,
-    0x5E,
-    0x5F,
-    0xF6,
-    0xF7,
-    0xF8,
-    0xF9,
-    0xFB,
-    0xFC,
-    0xFE,
-]
 
-# Gadget for invalid opcodes. It verifies invalid bytes in any condition of:
-# - `opcode > 0x20 && opcode < 0x30`
-# - `opcode > 0x48 && opcode < 0x50`
-# - `opcode > 0xA4 && opcode < 0xF0`
-# - one of [`SEPARATE_INVALID_OPCODES`]
+# Gadget for invalid opcodes. It verifies by a fixed lookup for ResponsibleOpcode.
 def invalid_opcode(instruction: Instruction):
     # Fixed lookup for invalid opcode.
     opcode = instruction.opcode_lookup(True)
     instruction.responsible_opcode_lookup(opcode)
-
-    op_gt_20, _ = instruction.compare(FQ(0x20), opcode, 1)
-    op_lt_30, _ = instruction.compare(opcode, FQ(0x30), 1)
-    op_gt_48, _ = instruction.compare(FQ(0x48), opcode, 1)
-    op_lt_50, _ = instruction.compare(opcode, FQ(0x50), 1)
-    op_gt_a4, _ = instruction.compare(FQ(0xA4), opcode, 1)
-    op_lt_f0, _ = instruction.compare(opcode, FQ(0xF0), 1)
-
-    op_range_20_30 = op_gt_20.n and op_lt_30.n
-    op_range_48_50 = op_gt_48.n and op_lt_50.n
-    op_range_a4_f0 = op_gt_a4.n and op_lt_f0.n
-
-    # Check separate byte set if no above condition is met.
-    if 1 - (op_range_20_30 or op_range_48_50 or op_range_a4_f0):
-        instruction.constrain_in(opcode, [FQ(i) for i in SEPARATE_INVALID_OPCODES])
 
     # Current call must be failed.
     is_success = instruction.call_context_lookup(CallContextFieldTag.IsSuccess)
