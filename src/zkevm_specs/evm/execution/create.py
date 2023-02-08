7453cf3eda3e65cab4c1789684cdbdb8acc65c4e
ashWhiteHat <phantomofrotten@gmail.com>
2023-02-08 14:08:31 +0900

    set contract state and transfer

--- src/zkevm_specs/evm/execution/create.py
+++ src/zkevm_specs/evm/execution/create.py
@@ -23,9 +23,20 @@ def create(instruction: Instruction):
 
     # ErrDepth constraint
     instruction.range_lookup(depth, CALL_CREATE_DEPTH)
+
     # ErrNonceUintOverflow constraint
     (is_not_overflow, _) = instruction.compare(nonce, nonce_prev, 8)
     instruction.is_zero(is_not_overflow)
 
     # add contract address to access list
     instruction.add_account_to_access_list(tx_id, contract_address)
+
+    # ErrContractAddressCollision constraint
+    code_hash = instruction.account_read(contract_address, AccountFieldTag.CodeHash)
+    instruction.is_zero(code_hash)
+
+    # init contract state
+    nonce, nonce_prev = instruction.account_write(contract_address, AccountFieldTag.Nonce)
+
+    # transfer value from caller to contract address
+    instruction.transfer(tx_caller_address, contract_address, value)
