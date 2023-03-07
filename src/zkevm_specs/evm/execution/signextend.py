4facdcb3a24e42a4631e6b8fd893d776bff8b323
ashWhiteHat <phantomofrotten@gmail.com>
2023-03-07 13:34:13 +0900

    add comments from original implementation

--- src/zkevm_specs/evm/execution/signextend.py
+++ src/zkevm_specs/evm/execution/signextend.py
@@ -9,6 +9,7 @@ def signextend(instruction: Instruction):
     value = instruction.stack_pop()
     result = instruction.stack_push()
 
+    # Any index value >= 256 always returns the same value
     is_msb_sum_zero = instruction.is_zero(FQ(sum(index.le_bytes[1:32])))
     sign_byte = (
         FQ((value.le_bytes[index.le_bytes[0]] >> 7) * 0xFF) if index.le_bytes[0] < 31 else FQ(0)
@@ -16,14 +17,26 @@ def signextend(instruction: Instruction):
     selectors = [FQ(index.expr() == FQ(i)) for i in range(31)]
     is_byte_selected = [FQ(index.le_bytes[0] == i) for i in range(31)]
 
+    # Check byte per byte to see if the byte was selected.
+    # We're only directly checking the LSB byte
+    # of the index here, so also make sure the byte
+    # is only copied when index < 256.
+    # There is no need to check the MSB, even if the MSB is selected
+    # no bytes need to be changed (so this loops only up to 31).
     selected_byte = FQ(0)
     for i in range(31):
         is_selected = is_byte_selected[i] * is_msb_sum_zero
         selected_byte += value.le_bytes[i] * is_selected
+        # Verify the selector
         instruction.is_equal(is_selected + (selectors[i - 1] if i > 0 else FQ(0)), selectors[i])
 
+    # Lookup the sign byte which will be used for doing the extending
     instruction.sign_byte_lookup(selected_byte, sign_byte)
 
+    # Byte 0 always remains the same.
+    # All other bytes need to be changed to the sign byte when the selector is enabled.
+    # When a byte was selected all the **following** bytes need to be replaced,
+    # (hence the `selectors[i-1]`).
     for idx in range(32):
         if idx == 0:
             instruction.is_equal(FQ(result.le_bytes[idx]), FQ(value.le_bytes[idx]))
