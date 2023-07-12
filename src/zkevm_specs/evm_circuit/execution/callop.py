8654a0b0ee31aaadf73bec7298dc41f2e2fc657f
KimiWu <leonhartwu@gmail.com>
2023-07-12 16:54:00 +0800

    feat: handle error insufficient balance in callop

--- src/zkevm_specs/evm_circuit/execution/callop.py
+++ src/zkevm_specs/evm_circuit/execution/callop.py
@@ -1,5 +1,5 @@
 from zkevm_specs.evm_circuit.util.call_gadget import CallGadget
-from zkevm_specs.util.param import N_BYTES_GAS
+from zkevm_specs.util.param import N_BYTES_GAS, N_BYTES_STACK
 from ...util import FQ, GAS_STIPEND_CALL_WITH_VALUE, Word, WordOrValue
 from ..instruction import Instruction, Transition
 from ..opcode import Opcode
@@ -22,9 +22,7 @@ def callop(instruction: Instruction):
         CallContextFieldTag.CalleeAddress
     )
     ctx_caller_address = instruction.word_to_address(ctx_caller_address_word)
-    is_static = instruction.select(
-        is_staticcall, FQ(1), instruction.call_context_lookup(CallContextFieldTag.IsStatic)
-    )
+    is_static = instruction.call_context_lookup(CallContextFieldTag.IsStatic)
     depth = instruction.call_context_lookup(CallContextFieldTag.Depth)
     parent_caller_address_word, parent_call_value = (
         (
@@ -34,12 +32,10 @@ def callop(instruction: Instruction):
         if is_delegatecall == 1
         else (Word(0), Word(0))
     )
-    parent_caller_address = instruction.word_to_address(parent_caller_address_word)
+    instruction.word_to_address(parent_caller_address_word)
 
-    # Verify depth is less than 1024
-    instruction.range_lookup(depth, 1024)
+    call = CallGadget(instruction, FQ(1), is_call, is_callcode, is_delegatecall, is_staticcall)
 
-    call = CallGadget(instruction, FQ(1), is_call, is_callcode, is_delegatecall)
     # For opcode CALLCODE:
     # - callee_address = caller_address
     #
@@ -61,7 +57,7 @@ def callop(instruction: Instruction):
         tx_id, call.callee_address, reversion_info
     )
 
-    # Check not is_static if call has value
+    # CALL with value must not be in static call stack
     has_value = call.has_value
     instruction.constrain_zero(has_value * is_static)
 
@@ -81,25 +77,27 @@ def callop(instruction: Instruction):
             reversion_info.rw_counter_of_reversion(),
         )
 
-    if is_call == 1:
-        # For CALL opcode, verify transfer, and get caller balance before
-        # transfer to constrain it should be greater than or equal to stack
-        # `value`.
+    ### Do stack depth and balance pre-check
+    insufficient_balance = FQ(0)
+    if is_call == FQ(1) or is_callcode == FQ(1):
+        caller_balance = instruction.account_read_word(caller_address, AccountFieldTag.Balance)
+        # ErrInsufficientBalance constraint
+        insufficient_balance, _ = instruction.compare_word(caller_balance, call.value)
+    is_depth_ok, _ = instruction.compare(depth, FQ(1025), N_BYTES_STACK)
+    is_precheck_ok = is_depth_ok == FQ(1) and insufficient_balance == FQ(0)
+
+    # If precheck is false, this call must be failed
+    if is_precheck_ok is False:
+        instruction.constrain_zero(call.is_success)
+
+    # For CALL opcode, transfer only when is_precheck_ok is true
+    if is_call == FQ(1) and is_precheck_ok == FQ(1):
         (_, caller_balance), _ = instruction.transfer(
             caller_address, callee_address, call.value, callee_reversion_info
         )
-    elif is_callcode == 1:
-        # For CALLCODE opcode, get caller balance to constrain it should be
-        # greater than or equal to stack `value`.
-        caller_balance = instruction.account_read_word(caller_address, AccountFieldTag.Balance)
-
-    # For both CALL and CALLCODE opcodes, verify caller balance is greater than
-    # or equal to stack `value`.
-    if is_call + is_callcode == 1:
-        value_lt_caller_balance, value_eq_caller_balance = instruction.compare_word(
-            call.value, caller_balance
-        )
-        instruction.constrain_zero(1 - value_lt_caller_balance - value_eq_caller_balance)
+    # For CALLCODE opcode, if is_success is true, then insufficient_balance must be zero
+    if is_callcode == FQ(1) and call.is_success == FQ(1):
+        instruction.constrain_zero(insufficient_balance)
 
     # Verify gas cost.
     gas_cost = call.gas_cost(
@@ -125,11 +123,9 @@ def callop(instruction: Instruction):
         is_precompile, FQ(instruction.next.execution_state in precompile_execution_states())
     )
 
+    stack_pointer_delta = 5 + is_call + is_callcode
     no_callee_code = call.is_empty_code_hash + call.callee_not_exists
-    if no_callee_code == FQ(1) and is_precompile == FQ(0):
-        # Make sure call is successful
-        instruction.constrain_equal(call.is_success, FQ(1))
-
+    if is_precheck_ok is False or (no_callee_code == FQ(1) and is_precompile == FQ(0)):
         # Empty return_data
         for field_tag, expected_value in [
             (CallContextFieldTag.LastCalleeId, FQ(0)),
@@ -141,15 +137,8 @@ def callop(instruction: Instruction):
                 expected_value,
             )
 
-        # For CALL opcode, it has an extra stack pop `value` and two account write for `transfer` call (+3).
-        # For CALLCODE opcode, it has an extra stack pop `value` and one account read for caller balance (+2).
-        # For DELEGATECALL opcode, it has two extra call context lookups for current caller address and value (+2).
-        # No extra lookups for STATICCALL opcode.
-        rw_counter_delta = 20 + is_call * 3 + is_callcode * 2 + is_delegatecall * 2
-        stack_pointer_delta = 5 + is_call + is_callcode
-
         instruction.constrain_step_state_transition(
-            rw_counter=Transition.delta(rw_counter_delta),
+            rw_counter=Transition.delta(instruction.rw_counter_offset),
             program_counter=Transition.delta(1),
             stack_pointer=Transition.delta(stack_pointer_delta),
             gas_left=Transition.delta(has_value * GAS_STIPEND_CALL_WITH_VALUE - gas_cost),
@@ -162,10 +151,6 @@ def callop(instruction: Instruction):
             code_hash=Transition.same_word(),
         )
     else:
-        # Similar as above comment.
-        rw_counter_delta = 40 + is_call * 3 + is_callcode * 2 + is_delegatecall * 2
-        stack_pointer_delta = 5 + is_call + is_callcode
-
         # Save caller's call state
         for field_tag, expected_value in [
             (CallContextFieldTag.ProgramCounter, instruction.curr.program_counter + 1),
@@ -222,7 +207,7 @@ def callop(instruction: Instruction):
         callee_gas_left += has_value * GAS_STIPEND_CALL_WITH_VALUE
 
         instruction.step_state_transition_to_new_context(
-            rw_counter=Transition.delta(rw_counter_delta),
+            rw_counter=Transition.delta(instruction.rw_counter_offset),
             call_id=Transition.to(callee_call_id),
             is_root=Transition.to(False),
             is_create=Transition.to(False),
