28998235eaf04bd8df253c067e88d705cec60ce7
Raphael <raphael.r.toledo@gmail.com>
2023-07-20 13:51:35 +0100

    Fully implement PUSHn (#464)
    * EvmCircuit/stop: supporting incomplete bytecode
    
    * EvmCircuit/push: updating specs to handle incomplete bytecodes

--- src/zkevm_specs/evm_circuit/execution/push.py
+++ src/zkevm_specs/evm_circuit/execution/push.py
@@ -1,3 +1,4 @@
+from ...util import N_BYTES_PROGRAM_COUNTER
 from ..instruction import Instruction, Transition
 from ..opcode import Opcode
 
@@ -5,15 +6,19 @@ from ..opcode import Opcode
 def push(instruction: Instruction):
     opcode = instruction.opcode_lookup(True)
     num_pushed = opcode - Opcode.PUSH1 + 1
-    num_additional_pushed = num_pushed - 1
+    code_length = instruction.bytecode_length(instruction.curr.code_hash)
+    code_length_left = code_length - instruction.curr.program_counter - 1
+    is_out_of_bound, _ = instruction.compare(code_length_left, num_pushed, N_BYTES_PROGRAM_COUNTER)
+    num_padding = is_out_of_bound * (num_pushed - code_length_left)
 
     value = instruction.stack_push()
     value_le_bytes = value.to_le_bytes()
-    selectors = instruction.continuous_selectors(num_additional_pushed, 31)
+    is_pushed = instruction.continuous_selectors(num_pushed, 32)
+    is_padding = instruction.continuous_selectors(num_padding, 32)
 
     for idx in range(32):
         index = instruction.curr.program_counter + num_pushed - idx
-        if idx == 0 or selectors[idx - 1] == 1:
+        if is_pushed[idx] * (1 - is_padding[idx]) == 1:
             instruction.constrain_equal(
                 value_le_bytes[idx], instruction.opcode_lookup_at(index, False)
             )
