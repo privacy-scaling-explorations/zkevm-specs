c96384854c8c453fcb9a0b9e3168a9b5e0ea79ce
KimiWu <leonhartwu@gmail.com>
2023-07-21 14:58:03 +0800

    using stack_lookup instead of stack_pop

--- src/zkevm_specs/evm_circuit/execution/error_oog_exp.py
+++ src/zkevm_specs/evm_circuit/execution/error_oog_exp.py
@@ -1,3 +1,4 @@
+from zkevm_specs.evm_circuit.table import RW
 from zkevm_specs.util.param import (
     GAS_COST_EXP_PER_BYTE,
     GAS_COST_SLOW,
@@ -9,21 +10,22 @@ from ...util import N_BYTES_GAS
 
 
 def error_oog_exp(instruction: Instruction):
-    # retrieve op code associated to oog constant error
+    # retrieve op code associated to oog exp error
     opcode = instruction.opcode_lookup(True)
     instruction.constrain_equal(opcode, Opcode.EXP)
 
-    instruction.stack_pop()
-    exponent = instruction.stack_pop()
+    exponent = instruction.stack_lookup(RW.Read, 1)
 
     # get total gas cost
     exponent_byte_size = instruction.byte_size(exponent)
     dynamic_gas_cost = GAS_COST_EXP_PER_BYTE * exponent_byte_size
 
     # check gas left is less than total gas required
-    gas_not_enough, _ = instruction.compare(
+    insufficient_gas, _ = instruction.compare(
         instruction.curr.gas_left, dynamic_gas_cost + GAS_COST_SLOW, N_BYTES_GAS
     )
-    instruction.constrain_equal(gas_not_enough, FQ(1))
+    instruction.constrain_equal(insufficient_gas, FQ(1))
 
-    instruction.constrain_error_state(instruction.rw_counter_offset + 1)
+    instruction.constrain_error_state(
+        instruction.rw_counter_offset + instruction.curr.reversible_write_counter + 1
+    )
--- tests/evm/test_error_oog_exp.py
+++ tests/evm/test_error_oog_exp.py
@@ -15,25 +15,25 @@ from zkevm_specs.util import Word
 
 POW2 = 2**256
 TESTING_DATA = (
-    (0, 0, 100),
-    (0, POW2 - 1, 100),
+    (0, 9),
+    (10, 59),
+    (POW2 - 1, 1609),
 )
 
 
-@pytest.mark.parametrize("base, exponent, gas_left", TESTING_DATA)
-def test_error_oog_exp(base: int, exponent: int, gas_left: int):
+@pytest.mark.parametrize("exponent, gas_left", TESTING_DATA)
+def test_error_oog_exp(exponent: int, gas_left: int):
     bytecode = Bytecode().exp()
     bytecode_hash = Word(bytecode.hash())
 
+    reversible_write_counter = 2
     current_call_id = 2
-    rw_counter = 14
-    rw_table = (
-        RWDictionary(rw_counter)
-        .stack_read(current_call_id, 1022, base)
-        .stack_read(current_call_id, 1023, exponent)
-    )
+    rw_counter = 13
+    pc = 1 * 33 + 1
     stack_pointer = 1022
-    pc = 2 * 33 + 1
+    rw_table = RWDictionary(rw_counter).stack_read(
+        current_call_id, stack_pointer + 1, Word(exponent)
+    )
 
     rw_table.call_context_read(current_call_id, CallContextFieldTag.IsSuccess, 0)
 
@@ -44,10 +44,10 @@ def test_error_oog_exp(base: int, exponent: int, gas_left: int):
         .call_context_read(1, CallContextFieldTag.IsCreate, False) \
         .call_context_read(1, CallContextFieldTag.CodeHash, bytecode_hash) \
         .call_context_read(1, CallContextFieldTag.ProgramCounter, pc) \
-        .call_context_read(1, CallContextFieldTag.StackPointer, stack_pointer) \
+        .call_context_read(1, CallContextFieldTag.StackPointer, 1023) \
         .call_context_read(1, CallContextFieldTag.GasLeft, gas_left) \
         .call_context_read(1, CallContextFieldTag.MemorySize, 0) \
-        .call_context_read(1, CallContextFieldTag.ReversibleWriteCounter, 0) \
+        .call_context_read(1, CallContextFieldTag.ReversibleWriteCounter, reversible_write_counter) \
         .call_context_write(1, CallContextFieldTag.LastCalleeId, 2) \
         .call_context_write(1, CallContextFieldTag.LastCalleeReturnDataOffset, 0) \
         .call_context_write(1, CallContextFieldTag.LastCalleeReturnDataLength, 0)
@@ -73,19 +73,19 @@ def test_error_oog_exp(base: int, exponent: int, gas_left: int):
                 program_counter=0,
                 stack_pointer=stack_pointer,
                 gas_left=gas_left,
-                reversible_write_counter=0,
+                reversible_write_counter=reversible_write_counter,
             ),
             StepState(
                 execution_state=ExecutionState.STOP,
-                rw_counter=rw_table.rw_counter,
+                rw_counter=rw_table.rw_counter + reversible_write_counter,
                 call_id=1,
                 is_root=False,
                 is_create=False,
                 code_hash=bytecode_hash,
                 program_counter=pc,
-                stack_pointer=stack_pointer,
+                stack_pointer=1023,
                 gas_left=gas_left,
-                reversible_write_counter=0,
+                reversible_write_counter=reversible_write_counter,
             ),
         ],
     )
