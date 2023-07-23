b193b60e0d5f910987c507a0d073ca9a10058439
Steven Gu <asongala@163.com>
2023-07-23 12:20:07 +0800

    Add `PUSH0`.

--- src/zkevm_specs/evm_circuit/execution/push.py
+++ src/zkevm_specs/evm_circuit/execution/push.py
@@ -5,7 +5,9 @@ from ..opcode import Opcode
 
 def push(instruction: Instruction):
     opcode = instruction.opcode_lookup(True)
-    num_pushed = opcode - Opcode.PUSH1 + 1
+    is_push0 = instruction.is_equal(opcode, Opcode.PUSH0)
+
+    num_pushed = opcode - Opcode.PUSH0
     code_length = instruction.bytecode_length(instruction.curr.code_hash)
     code_length_left = code_length - instruction.curr.program_counter - 1
     is_out_of_bound, _ = instruction.compare(code_length_left, num_pushed, N_BYTES_PROGRAM_COUNTER)
@@ -16,9 +18,12 @@ def push(instruction: Instruction):
     is_pushed = instruction.continuous_selectors(num_pushed, 32)
     is_padding = instruction.continuous_selectors(num_padding, 32)
 
+    # Constrain stack write value must be zero for PUSH0.
+    instruction.constrain_zero(is_push0 * instruction.sum(value_le_bytes))
+
     for idx in range(32):
-        index = instruction.curr.program_counter + num_pushed - idx
         if is_pushed[idx] * (1 - is_padding[idx]) == 1:
+            index = instruction.curr.program_counter + num_pushed - idx
             instruction.constrain_equal(
                 value_le_bytes[idx], instruction.opcode_lookup_at(index, False)
             )
