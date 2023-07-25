01fd6871dad215feb248cebecee80c5adf6e1794
Steven Gu <asongala@163.com>
2023-07-25 08:37:02 +0800

    Delete `value == 0` constraint for PUSH0 (has already constrained).

--- src/zkevm_specs/evm_circuit/execution/push.py
+++ src/zkevm_specs/evm_circuit/execution/push.py
@@ -5,8 +5,6 @@ from ..opcode import Opcode
 
 def push(instruction: Instruction):
     opcode = instruction.opcode_lookup(True)
-    is_push0 = instruction.is_equal(opcode, Opcode.PUSH0)
-
     num_pushed = opcode - Opcode.PUSH0
     code_length = instruction.bytecode_length(instruction.curr.code_hash)
     code_length_left = code_length - instruction.curr.program_counter - 1
@@ -17,9 +15,6 @@ def push(instruction: Instruction):
     value_le_bytes = value.to_le_bytes()
     is_pushed = instruction.continuous_selectors(num_pushed, 32)
     is_padding = instruction.continuous_selectors(num_padding, 32)
-
-    # Constrain stack write value must be zero for PUSH0.
-    instruction.constrain_zero(is_push0 * instruction.sum(value_le_bytes))
 
     for idx in range(32):
         if is_pushed[idx] * (1 - is_padding[idx]) == 1:
