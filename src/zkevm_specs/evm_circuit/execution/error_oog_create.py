9bfd15cad026d96d4b75ce5d1744393ffb5edd14
KimiWu <leonhartwu@gmail.com>
2023-08-04 15:27:09 +0800

    feat: support contract creation in error_oog_create

--- src/zkevm_specs/evm_circuit/execution/error_oog_create.py
+++ src/zkevm_specs/evm_circuit/execution/error_oog_create.py
@@ -3,14 +3,17 @@ from zkevm_specs.util.arithmetic import FQ
 from zkevm_specs.util.param import (
     GAS_COST_COPY_SHA3,
     GAS_COST_CREATE,
+    GAS_COST_CREATION_TX,
     GAS_COST_INITCODE_WORD,
+    GAS_COST_TX_CALL_DATA_PER_NON_ZERO_BYTE,
+    GAS_COST_TX_CALL_DATA_PER_ZERO_BYTE,
     MAX_INIT_CODE_SIZE,
     N_BYTES_GAS,
     N_BYTES_MEMORY_WORD_SIZE,
     N_BYTES_U64,
 )
 from ..instruction import Instruction
-from ..table import RW
+from ..table import RW, CallContextFieldTag
 
 
 def error_oog_create(instruction: Instruction):
@@ -26,19 +29,33 @@ def error_oog_create(instruction: Instruction):
     size_word = instruction.stack_lookup(RW.Read, 2)
     offset, size = instruction.memory_offset_and_length(offset_word, size_word)
 
-    (_, memory_expansion_gas_cost) = instruction.memory_expansion(offset, size)
-    word_size, _ = instruction.constant_divmod(size + FQ(31), FQ(32), N_BYTES_MEMORY_WORD_SIZE)
+    is_root = instruction.call_context_lookup(CallContextFieldTag.IsRoot)
 
-    # ErrMaxInitCodeSizeExceeded
-    is_exceed_max_initcode_size, _ = instruction.compare(FQ(MAX_INIT_CODE_SIZE), size, N_BYTES_U64)
+    # gas cost calculation
+    if is_root == FQ(1):
+        tx_id = instruction.call_context_lookup(CallContextFieldTag.TxId)
+        data = [instruction.tx_calldata_lookup(tx_id, FQ(idx)) for idx in range(0, size.n)]
+        data_len = len(data)
 
-    # gas cost
+        # gas cost, 16 gas per non-zero byte, 4 gas per zero byte
+        gas_cost = GAS_COST_CREATION_TX
+        nz = len([byte for byte in data if byte != 0])
+        gas_cost += nz * GAS_COST_TX_CALL_DATA_PER_NON_ZERO_BYTE
+        z = data_len - nz
+        gas_cost += z * GAS_COST_TX_CALL_DATA_PER_ZERO_BYTE
+    else:
+        (_, memory_expansion_gas_cost) = instruction.memory_expansion(offset, size)
+        gas_cost = GAS_COST_CREATE + memory_expansion_gas_cost
+
     # GAS_COST_INITCODE_WORD * word_size introduced in EIP-3860
-    gas_cost = GAS_COST_CREATE + memory_expansion_gas_cost + GAS_COST_INITCODE_WORD * word_size
-
+    word_size, _ = instruction.constant_divmod(size + FQ(31), FQ(32), N_BYTES_MEMORY_WORD_SIZE)
+    gas_cost += GAS_COST_INITCODE_WORD * word_size
     # gas cost for SHA3 calculation if it's a CREATE2 opcode
     if is_create2 == FQ(1):
         gas_cost += GAS_COST_COPY_SHA3 * word_size
+
+    # ErrMaxInitCodeSizeExceeded
+    is_exceed_max_initcode_size, _ = instruction.compare(FQ(MAX_INIT_CODE_SIZE), size, N_BYTES_U64)
 
     insufficient_gas, _ = instruction.compare(instruction.curr.gas_left, gas_cost, N_BYTES_GAS)
 
--- tests/evm/test_error_oog_create.py
+++ tests/evm/test_error_oog_create.py
@@ -10,29 +10,47 @@ from zkevm_specs.evm_circuit import (
     Bytecode,
     RWDictionary,
     Opcode,
+    Transaction,
 )
 from zkevm_specs.util import Word
 from zkevm_specs.util.param import MAX_INIT_CODE_SIZE
 
 
+def gen_calldata(bytes_len: int) -> bytes:
+    calldata = []
+    calldata.extend([int(Opcode.PUSH32)] * bytes_len)
+    assert len(calldata) == bytes_len
+    return bytes(calldata)
+
+
 TESTING_DATA_IS_ROOT = (
     # gas cost: 32000 + 60 (memory expansion) + (320/32) * 2 = 32080
     # CREATE2 has extra keccak gas cost = (320/32) * 6 = 60
-    (Opcode.CREATE, 320, 320, 32079),
-    (Opcode.CREATE2, 320, 320, 32139),
-    (Opcode.CREATE, 320, 320, 32079),
-    (Opcode.CREATE2, 320, 320, 32139),
+    (Opcode.CREATE, 320, 320, 32079, False),
+    (Opcode.CREATE2, 320, 320, 32139, False),
     # bytecode size exceeds MAX_INIT_CODE_SIZE
-    (Opcode.CREATE, 0, MAX_INIT_CODE_SIZE + 1, int(5e6)),
-    (Opcode.CREATE2, 0, MAX_INIT_CODE_SIZE + 1, int(5e6)),
+    (Opcode.CREATE, 0, MAX_INIT_CODE_SIZE + 1, int(5e6), False),
+    (Opcode.CREATE2, 0, MAX_INIT_CODE_SIZE + 1, int(5e6), False),
+    # hit insufficient gas and MAX_INIT_CODE_SIZE
+    (Opcode.CREATE, 0, MAX_INIT_CODE_SIZE + 1, int(3e6), False),
+    (Opcode.CREATE2, 0, MAX_INIT_CODE_SIZE + 1, int(3e6), False),
+    ## Same cases, with is_root set to True
+    # gas cost: 53000 + 320 * 16 + (320/32) * 2 = 58140
+    (Opcode.CREATE, 320, 320, 58139, True),
+    (Opcode.CREATE2, 320, 320, 58199, True),
+    # To generate a size of (MAX_INIT_CODE_SIZE + 1) calldata would cause memory corrupted in the local env.
+    # So, to make ci and local test works, we ignore the cases if overflowed calldata needed.
+    # Those cases were verified by changing MAX_INIT_CODE_SIZE to a smaller number, like 320
+    # (Opcode.CREATE, 0, MAX_INIT_CODE_SIZE + 1, int(5e6), True),
+    # (Opcode.CREATE2, 0, MAX_INIT_CODE_SIZE + 1, int(5e6), True),
 )
 
 
-@pytest.mark.parametrize("opcode, offset, length, gas_left", TESTING_DATA_IS_ROOT)
-def test_error_oog_create(opcode: Opcode, offset: int, length: int, gas_left: int):
+@pytest.mark.parametrize("opcode, offset, length, gas_left, is_root", TESTING_DATA_IS_ROOT)
+def test_error_oog_create(opcode: Opcode, offset: int, length: int, gas_left: int, is_root: bool):
     reversible_write_counter = 2
     rw_counter = 14
-    caller_id = 2
+    caller_id = 1 if is_root is True else 2
     is_create2 = opcode == Opcode.CREATE2
     if is_create2:
         bytecode = Bytecode().push32(1).push32(offset).push32(length).push32(123).create2()
@@ -49,27 +67,33 @@ def test_error_oog_create(opcode: Opcode, offset: int, length: int, gas_left: in
     )
     bytecode_hash = Word(bytecode.hash())
 
+    rw_table.call_context_read(caller_id, CallContextFieldTag.IsRoot, is_root)
+    if is_root is True:
+        tx = Transaction(id=1, call_data=gen_calldata(length if length < MAX_INIT_CODE_SIZE else 1))
+        rw_table.call_context_read(caller_id, CallContextFieldTag.TxId, 1)
+
     rw_table.call_context_read(caller_id, CallContextFieldTag.IsSuccess, 0)
 
-    # fmt: off
-    rw_table \
-        .call_context_read(2, CallContextFieldTag.CallerId, 1) \
-        .call_context_read(1, CallContextFieldTag.IsRoot, False) \
-        .call_context_read(1, CallContextFieldTag.IsCreate, True) \
-        .call_context_read(1, CallContextFieldTag.CodeHash, bytecode_hash) \
-        .call_context_read(1, CallContextFieldTag.ProgramCounter, pc + 1) \
-        .call_context_read(1, CallContextFieldTag.StackPointer, 1024) \
-        .call_context_read(1, CallContextFieldTag.GasLeft, 0) \
-        .call_context_read(1, CallContextFieldTag.MemorySize, 0) \
-        .call_context_read(1, CallContextFieldTag.ReversibleWriteCounter, reversible_write_counter) \
-        .call_context_write(1, CallContextFieldTag.LastCalleeId, 2) \
-        .call_context_write(1, CallContextFieldTag.LastCalleeReturnDataOffset, 0) \
-        .call_context_write(1, CallContextFieldTag.LastCalleeReturnDataLength, 0)
-    # fmt: on
+    if is_root is False:
+        # fmt: off
+        rw_table \
+            .call_context_read(caller_id, CallContextFieldTag.CallerId, 1) \
+            .call_context_read(1, CallContextFieldTag.IsRoot, False) \
+            .call_context_read(1, CallContextFieldTag.IsCreate, True) \
+            .call_context_read(1, CallContextFieldTag.CodeHash, bytecode_hash) \
+            .call_context_read(1, CallContextFieldTag.ProgramCounter, pc + 1) \
+            .call_context_read(1, CallContextFieldTag.StackPointer, 1024) \
+            .call_context_read(1, CallContextFieldTag.GasLeft, 0) \
+            .call_context_read(1, CallContextFieldTag.MemorySize, 0) \
+            .call_context_read(1, CallContextFieldTag.ReversibleWriteCounter, reversible_write_counter) \
+            .call_context_write(1, CallContextFieldTag.LastCalleeId, 2) \
+            .call_context_write(1, CallContextFieldTag.LastCalleeReturnDataOffset, 0) \
+            .call_context_write(1, CallContextFieldTag.LastCalleeReturnDataLength, 0)
+        # fmt: on
 
     tables = Tables(
         block_table=set(Block().table_assignments()),
-        tx_table=set(),
+        tx_table=set(tx.table_assignments()) if is_root is True else set(),
         bytecode_table=set(bytecode.table_assignments()),
         rw_table=set(rw_table.rws),
     )
@@ -81,7 +105,7 @@ def test_error_oog_create(opcode: Opcode, offset: int, length: int, gas_left: in
                 execution_state=ExecutionState.ErrorOutOfGasCREATE2,
                 rw_counter=rw_counter,
                 call_id=caller_id,
-                is_root=False,
+                is_root=is_root,
                 is_create=True,
                 code_hash=bytecode_hash,
                 program_counter=pc,
@@ -90,10 +114,16 @@ def test_error_oog_create(opcode: Opcode, offset: int, length: int, gas_left: in
                 reversible_write_counter=reversible_write_counter,
             ),
             StepState(
+                execution_state=ExecutionState.EndTx,
+                rw_counter=rw_table.rw_counter + reversible_write_counter,
+                call_id=1,
+            )
+            if is_root is True
+            else StepState(
                 execution_state=ExecutionState.STOP,
                 rw_counter=rw_table.rw_counter + reversible_write_counter,
                 call_id=1,
-                is_root=False,
+                is_root=is_root,
                 is_create=True,
                 code_hash=bytecode_hash,
                 program_counter=pc + 1,
